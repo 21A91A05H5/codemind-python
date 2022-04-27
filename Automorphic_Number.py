df16b8727af3b3fee28067862deb0e0bae9e692c
21A91A05H5 <97424513+21A91A05H5@users.noreply.github.com>
2022-04-27 22:32:18 +0530

    a=int(input()) i=len(str(a)) sq=a**2 last=sq%pow(10,i) if last==a:     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,16 +1,8 @@
-num = int(input())  
-  
-#calculating the number of digits  
-num_of_digits = len(str(num))  
-  
-#computing the square of a number  
-square = num**2  
-  
-#obtaining the last digits   
-last_digits = square%pow(10,num_of_digits)  
-  
-#comparing the digits of number with input  
-if last_digits == num:  
-  print('Automorphic Number')  
-else:  
-  print('Not an Automorphic Number')
+a=int(input())
+i=len(str(a))
+sq=a**2
+last=sq%pow(10,i)
+if last==a:
+    print("Automorphic Number")
+else:
+    print("Not an Automorphic Number")
