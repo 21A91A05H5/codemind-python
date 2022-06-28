7d0695b902a5e0f9cf9967d08d33838f86c868d3
Sakethram_421 <97424513+21A91A05H5@users.noreply.github.com>
2022-06-28 22:04:25 +0530

    # import math n= int(input()) x=n**0.5 # root = math.sqrt(number) # if int(root + 0.9999) ** 2 == number: #     print("True") # else: #     print("False") if n%x==0:     print("True") else:     print("False")

--- perfect_square_root_or_not.py
+++ perfect_square_root_or_not.py
@@ -1,7 +1,12 @@
-import math
-number = int(input())
-root = math.sqrt(number)
-if int(root + 0.9999) ** 2 == number:
+# import math
+n= int(input())
+x=n**0.5
+# root = math.sqrt(number)
+# if int(root + 0.9999) ** 2 == number:
+#     print("True")
+# else:
+#     print("False")
+if n%x==0:
     print("True")
 else:
     print("False")
