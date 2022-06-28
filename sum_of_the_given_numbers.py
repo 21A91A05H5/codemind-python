d00b5730fd18f894d895879fefcd5aa311e77c47
Sakethram_421 <97424513+21A91A05H5@users.noreply.github.com>
2022-06-28 22:12:18 +0530

    t=int(input()) for i in range(t):     a,b=map(int,input().split())     print(a+b)

--- sum_of_the_given_numbers.py
+++ sum_of_the_given_numbers.py
@@ -1,5 +1,4 @@
-n=int(input())
-for i in range(1,n+1):
-    m,n=map(int,input().split( ))
-    s=m+n
-    print(s)
+t=int(input())
+for i in range(t):
+    a,b=map(int,input().split())
+    print(a+b)
