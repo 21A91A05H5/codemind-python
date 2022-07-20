b1fd321e83335de9128514aea92a0205abccb9d4
Sakethram_421 <97424513+21A91A05H5@users.noreply.github.com>
2022-07-20 22:44:39 +0530

    s=input() c1=s.count('a') c2=s.count('e') c3=s.count('i') c4=s.count('o') c5=s.count('u') if c1==0 or c2==0 or c3==0 or c4==0 or c5==0:     print('False') else:     print('True')

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,14 +1,10 @@
-n=input()
-s="aeiou"
-l=[]
-for i in n:
-    if i in s:
-        l.append(i)
-a=set(l)
-if(len(s)==len(a)):
-    print("True")
+s=input()
+c1=s.count('a')
+c2=s.count('e')
+c3=s.count('i')
+c4=s.count('o')
+c5=s.count('u')
+if c1==0 or c2==0 or c3==0 or c4==0 or c5==0:
+    print('False')
 else:
-    print("False")
-
-
-        
+    print('True')
