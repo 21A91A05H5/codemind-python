1d825d2e346a57d6a51117a7b12b83aae5304b0d
Sakethram_421 <97424513+21A91A05H5@users.noreply.github.com>
2022-08-24 14:56:28 +0530

    s=input().split() l=[] for i in sorted(s):     x=len(i)     l.append(x) a=min(l) b=max(l) w=[] while a<=b:     c=0     for i in sorted(s):         if len(i)==a:             w.append(i)     a+=1 print(' '.join(w)) # print(len(w))

---
+++ sort_the_words_based_on_length.py
@@ -0,0 +1,17 @@
+
+s=input().split()
+l=[]
+for i in sorted(s):
+    x=len(i)
+    l.append(x)
+a=min(l)
+b=max(l)
+w=[]
+while a<=b:
+    c=0
+    for i in sorted(s):
+        if len(i)==a:
+            w.append(i)
+    a+=1
+print(' '.join(w))
+# print(len(w))
