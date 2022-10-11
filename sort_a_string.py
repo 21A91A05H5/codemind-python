7fe894466caa527c30aac94347fe972d90ad13be
Sakethram_421 <97424513+21A91A05H5@users.noreply.github.com>
2022-10-11 11:51:23 +0530

    a=input() l=[] b='' c=[] z=[] for i in range(len(a)):     if a[i].isalnum():         b+=a[i]     else:         l.append(i)         z.append(a[i]) b=sorted(b) for i in range(len(l)):     b.insert(l[i],z[i]) print(''.join(b))

--- sort_a_string.py
+++ sort_a_string.py
@@ -1,14 +1,15 @@
-s=[]
-x=[]
-q=[]
-a=list(input())
+a=input()
+l=[]
+b=''
+c=[]
+z=[]
 for i in range(len(a)):
-    if a[i].isalpha():
-        s.append(a[i])
+    if a[i].isalnum():
+        b+=a[i]
     else:
-        x.append(i)
-        q.append(a[i])
-s=sorted(s)
-for i in range(len(x)):
-    s.insert(x[i],q[i])
-print("".join(s))
+        l.append(i)
+        z.append(a[i])
+b=sorted(b)
+for i in range(len(l)):
+    b.insert(l[i],z[i])
+print(''.join(b))
